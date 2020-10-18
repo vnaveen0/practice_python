46d1c46bb392cbf5f53be0021b83b17bd8dda726
vnaveen0 <vnaveen0@gmail.com>
2020-10-17 18:59:03 -0700

    adding mergeTwoLists

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -6,99 +6,66 @@
 class Solution(object):
 
     def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        start1 = l1
-        start2 = l2
+        def update_ptr(ptr, l):
+            ptr.val = l.val
+            ptr.next = None
+            return ptr
+
+        prev_ptr = None
+        start_ptr = prev_ptr
         if l1 is None:
             return l2
+
         if l2 is None:
             return l1
 
-        while (l1 is not None or l2 is not None):
+        while l1 is not None or l2 is not None:
+            cur_ptr = ListNode()
             if l1 is None:
-                return l2
+                prev_ptr.next = l2
+                return start_ptr
             if l2 is None:
-                return l1
+                prev_ptr.next = l1
+                return start_ptr
 
-            tmp1, tmp2 = self.merge(l1, l2)
-            print(tmp1, tmp2)
-
-        if start1.val <= start2.val:
-            return start1
-        else:
-            return start2
-
-    def merge(self, l1, l2):
-        if l1.next is None and l2.next is None:
             if l1.val <= l2.val:
-                l1.next = l2
+                cur_ptr = update_ptr(cur_ptr, l1)
+                l1 = l1.next
             else:
-                l2.next = l1
+                cur_ptr = update_ptr(cur_ptr, l2)
+                l2 = l2.next
 
-            return None, None
-
-        elif l1.next is None and l2.next is not None:
-            t2 = l2.next
-            tmp2 = l2.next.next
-            if l1.val <= l2.val:
-                l1.next = l2
+            if prev_ptr is None:
+                prev_ptr = cur_ptr
+                start_ptr = prev_ptr
             else:
-                l2.next = l1
-                l2.next.next = t2
-            return None, tmp2
+                prev_ptr.next = cur_ptr
+                prev_ptr = prev_ptr.next
 
-        elif l2.next is None and l1.next is not None:
-            t1 = l1.next
-            tmp1 = l1.next.next
-            if l1.val <= l2.val:
-                l1.next = l2
-                l1.next.next = t1
-            else:
-                l2.next = l1
+        return start_ptr
 
-            return tmp1, None
-        else:
-            # l1.next is not None and l2.next is not None
-            tmp1 = l1.next.next
-            tmp2 = l2.next.next
-            t1 = l1.next
-            t2 = l2.next
-            v1_next = l1.next.val
-            v2_next = l2.next.val
-            v1 = l1.val
-            v2 = l2.val
-            if v1_next <= v2:
-                # 2: v1, v1',v2,v2'
-                l1.next.next = l2
 
-            elif v2_next <= v1:
-                # 3: v2,v2',v1,v1'
-                l2.next.next = v1
 
-            elif v1 <= v2:
-                # 1: v1, v2, v1',v2'
-                l1.next = l2
-                l1.next.next = t1
-                l1.next.next.next = t2
 
-            elif v2 <= v1:
-                # 4: v2, v1, v2',v1'
-                l1 = l2
-                l1.next = v1
-                l1.next.next = t2
-                l1.next.next.next = t1
 
-            return tmp1, tmp2
 
+        return start_ptr
 
 class ListNode(object):
-    def __init__(self, val, ptr):
+    def __init__(self, val=None, ptr=None):
         self.val = val
         self.next = ptr
+
+    def __str__(self):
+        a = []
+        start = self
+        if start is None:
+            return a.__str__()
+        while(start is not None):
+            a.append(start.val)
+            start = start.next
+
+        return a.__str__()
 
 
 class LL(object):
@@ -126,19 +93,18 @@ class LL(object):
         print(a)
 
 
-
-
 if __name__ == '__main__':
     s = Solution()
-    ar1 = [1,2]
-    ar2 = [1]
+    ar1 = []
+    ar2 = [4]
 
     l1 = LL(ar1)
     l2 = LL(ar2)
     l1.print()
     l2.print()
 
-    ans = s.mergeTwoLists(l1.head,l2.head)
+    ans = s.mergeTwoLists(l1.head, l2.head)
+    print(ans)
 
 
 
