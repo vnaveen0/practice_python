bee626af3210c288994ced88193e8711742f765f
vnaveen0 <vnaveen0@gmail.com>
2021-05-01 15:23:11 -0700

    added urlify

---
+++ String/urlify.py
@@ -0,0 +1,19 @@
+# Replace space with %20
+# Ignore spaces in the end.
+
+# String is immutable in python..
+str = 'Mr  John Smith    '
+
+str2 = ''
+isfirst = True
+for s in reversed(str):
+    if s == ' ' and isfirst:
+        continue
+
+    elif s == ' ':
+        str2 = '%20' + str2
+    else:
+        isfirst = False
+        str2 = s + str2
+        
+print(str2)
